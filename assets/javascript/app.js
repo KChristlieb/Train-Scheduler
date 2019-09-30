1a27c2cfb5b3980a1517ecc7ca971fef220a6b65
Kade Christlieb <kchristlieb@gmail.com>
2019-09-30 12:58:40 -0600

    added a moment.js file to javascript directory. I added all tags in the html to call to external files and code. Added config script to app.js to tie into firebase. Copied code from moment js to monet.js file for the moment calls in JS.

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyCZZqGEaka5ohH-Tr-dzDEMuZRd9ut1C8A",
+    authDomain: "train-time-f94f6.firebaseapp.com",
+    databaseURL: "https://train-time-f94f6.firebaseio.com",
+    projectId: "train-time-f94f6",
+    storageBucket: "",
+    messagingSenderId: "861965481230",
+    appId: "1:861965481230:web:01f5c25a1e48e645fad35f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
